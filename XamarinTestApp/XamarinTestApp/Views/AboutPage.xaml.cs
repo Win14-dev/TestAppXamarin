87e374fe6f14ef7acd69f04d48fc4ff5a25c2aa1
Bhautik B <bhautik@moontechnolabs.com>
2021-06-09 15:28:25 +0530

    First commit

---
+++ XamarinTestApp/XamarinTestApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using Xamarin.CommunityToolkit.UI.Views;
+
+namespace XamarinTestApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            try
+            {
+                InitializeComponent();
+                VideoPlayu();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        async void VideoPlayu()
+        {
+            try
+            {
+                YoutubeClient youtube = new YoutubeClient();
+
+                StreamManifest streamManifest = await youtube.Videos.Streams.GetManifestAsync("https://www.youtube.com/watch?v=3jCsZBEuvvw");
+
+                IVideoStreamInfo streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+                if (streamInfo != null)
+                {
+                    Media.Source = streamInfo.Url;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
